79d8b684d003ec1e786286c8b03a637928882422
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-27 17:08:57 -0400

    Launcher3: Override QuickSearchBox
    * We track QuickSearchBox for EPPE, but don't want it built.
    
    Change-Id: I3b12b6493c3a191f3dabd1d3537da403a2c38349

--- Android.mk
+++ Android.mk
@@ -39,7 +39,7 @@ LOCAL_MIN_SDK_VERSION := 26
 LOCAL_PACKAGE_NAME := Launcher3Go
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep QuickSearchBox
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_awaken_launcher3.xml
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
@@ -74,7 +74,7 @@ LOCAL_PACKAGE_NAME := Launcher3QuickStep
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 QuickSearchBox
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_awaken_launcher3.xml
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/quickstep/res
@@ -128,7 +128,7 @@ LOCAL_PROGUARD_ENABLED := full
 LOCAL_PACKAGE_NAME := Launcher3QuickStepGo
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep QuickSearchBox
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3 privapp_whitelist_awaken_launcher3.xml
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
